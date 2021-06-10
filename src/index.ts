e69317fee12c038192b75fcea9af27315030fe21
czM1K3 <misamadera1@gmail.com>
2021-06-10 17:44:06 +0200

    GraphQL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import * as express from "express";
+import express from "express";
+import typeDefs from "./graphql/typedefs";
+import resolvers from "./graphql/resolvers";
+import { ApolloServer } from "apollo-server-express";
 
 const app = express();
 const port = 8080;
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+//@ts-ignore
+server.applyMiddleware({app, path: "/graphql"});
 
 app.get("/", (req,res) => {
 	res.send("Hello world!!!");
